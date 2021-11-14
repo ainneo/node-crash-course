c30cd4e3bef1ebde01a7691a2dea0812930d11e7
ainneo <ainneo@gmail.com>
2021-11-13 22:45:13 -0800

    req, res objs, routing and html pgs

--- server.js
+++ server.js
@@ -1,45 +1,52 @@
-const http = require('http');
-const fs = require('fs');
+const http = require("http");
+const fs = require("fs");
 
+//create a server
 const server = http.createServer((req, res) => {
-  // console.log(req);
-  console.log(req.url);
+  // console.log(req); // prints to terminal all methods/props of the req obj
+  // console.log(req.url); // prints / that is our url
+  // console.log(req.method); //req.method logs GET method to terminal
 
-  // set header content type
-  res.setHeader('Content-Type', 'text/html');
+  // //The res obj: sends the a response to the browser
+  // res.setHeader("Content-Type", "text/html"); // set header content type
+  // res.setHeader("X-Foo", "bar");
+  // res.write("<p>hello, ninjas</p>"); //writes to the browser
+  // res.write("<p>hello again, ninjas</p>"); //writes to the browser again
+  // res.end(); //ends the response
+  // //Notes: not the best way to display the HTML response to browser
+  // //best way is to use a full html file
+  // //html file has been created in the views folder
 
-  // res.write('<p>hello, ninjas</p>');
-  // res.write('<p>hello again, ninjas</p>');
-  // res.end();
-
-  // send html file
-  // fs.readFile('./views/index.html', (err, data) => {
+  // // send html file
+  // fs.readFile("./views/index.html", (err, data) => {
   //   if (err) {
   //     console.log(err);
   //     res.end();
   //   }
-  //   //res.write(data);
+  //   //res.write(data); //dont always need res.write if there is only a few res, we can directly pass data into end
   //   res.end(data);
   // });
 
-  // routing
-  let path = './views/';
-  switch(req.url) {
-    case '/':
-      path += 'index.html';
+  // basic routing
+  // switch statement to route the requests
+  // if the switch statment matches the case, then we are going to cancatenate paths
+  let path = "./views/";
+  switch (req.url) {
+    case "/":
+      path += "index.html";
       res.statusCode = 200;
       break;
-    case '/about':
-      path += 'about.html';
+    case "/about":
+      path += "about.html";
       res.statusCode = 200;
       break;
-    case '/about-us':
+    case "/about-us":
       res.statusCode = 301;
-      res.setHeader('Location', '/about');
+      res.setHeader("Location", "/about"); //redirects to /about
       res.end();
       break;
     default:
-      path += '404.html';
+      path += "404.html";
       res.statusCode = 404;
   }
 
@@ -49,14 +56,12 @@ const server = http.createServer((req, res) => {
       console.log(err);
       res.end();
     }
-    //res.write(data);
+    //res.write(data);//dont always need res.write if there is only a few res, we can directly pass data into end
     res.end(data);
   });
-
-
 });
 
 // localhost is the default value for 2nd argument
-server.listen(3000, 'localhost', () => {
-  console.log('listening for requests on port 3000');
+server.listen(3000, "localhost", () => {
+  console.log("listening for requests on port 3000");
 });
