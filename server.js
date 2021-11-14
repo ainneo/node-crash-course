864547997a5f29936700e844865b2ab6bc94db91
ainneo <ainneo@gmail.com>
2021-11-13 17:10:44 -0800

    client & server

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
-const http = require('http');
+const http = require("http");
 
+// this method creates a server
 const server = http.createServer((req, res) => {
-  console.log('request made');
+  console.log("request made"); //logs in terminal, not browser, it's not on frontend it's on server which is the backend
 });
 
 // localhost is the default value for 2nd argument
-server.listen(3000, 'localhost', () => {
-  console.log('listening for requests on port 3000');
+// this method starts the server and listens for requests
+server.listen(3000, "localhost", () => {
+  console.log("listening for requests on port 3000");
 });
+
+//Notes:
+//Lochost is like a domain name on the web.
+//it is a loopback to our own computer
+//localhost  listens for request coming in our computer
+//Port numbers are a gateway/port on our computer that a bit of software can comunicat with.
